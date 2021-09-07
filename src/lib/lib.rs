a4d8dddde8b0c8496e4c1c877e65edbf0a610059
Mark Musante <mark.musante@gmail.com>
2021-09-06 21:24:50 -0400

    refactor components into their own file

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,31 +1,9 @@
-use specs::prelude::*;
-use specs_derive::Component;
 
 pub mod map;
 pub mod monster_ai_system;
 pub mod rect;
 pub mod visibility_system;
 
-#[derive(Component)]
-pub struct Viewshed {
-    pub visible_tiles: Vec<rltk::Point>,
-    pub range: i32,
-    pub dirty: bool,
-}
+pub mod components;
 
-#[derive(Component)]
-pub struct Position {
-    pub x: i32,
-    pub y: i32,
-}
-
-#[derive(Component, Debug)]
-pub struct Player {}
-
-#[derive(Component, Debug)]
-pub struct Monster {}
-
-#[derive(Component, Debug)]
-pub struct Name {
-    pub name: String,
-}
+pub use components::{Viewshed, Position, Player, Monster, Name};
